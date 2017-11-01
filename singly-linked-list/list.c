9b555066e0c2665ef015f565f6b0a80b7c550729
Gu haihong <guhaihong128@gmail.com>
2017-11-01 18:31:58 +0800

    sync

---
+++ singly-linked-list/list.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "list.h"
+// #include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    Node node;
+    node.value = 1;
+
+    printf("%d\n", node.value);
+    return 0;
+}
+
+int len(Node node)
+{
+    int length = 0;
+
+    return length;
+}
---
+++ singly-linked-list/list.h
@@ -0,0 +1,16 @@
+
+typedef struct Nodes
+{
+    int value;
+    struct Node *next; 
+} Node;
+
+// length
+int len(Node node);
+// insert
+// void insert(Node *node, int value);
+// delete
+
+// find
+
+// isEmpty
