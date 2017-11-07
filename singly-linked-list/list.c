3df1147e7792becd5fd51c8aeb6e80093a9a425b
valley <valley.gu@adexpress.com>
2017-11-07 18:41:51 +0800

    sync

--- singly-linked-list/list.c
+++ singly-linked-list/list.c
@@ -15,6 +15,7 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
+
 // List MakeEmpty(List L)
 // {
 //     L = NULL;
--- singly-linked-list/list.h
+++ singly-linked-list/list.h
@@ -1,5 +1,7 @@
 #ifndef _List_H
 
+#define _List_H
+
 struct Node;
 typedef struct Node *PtrToNode;
 typedef PtrToNode List;
